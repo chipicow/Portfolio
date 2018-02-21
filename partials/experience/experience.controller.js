d32ce04833e8f926c7fda097a5b4a7614f5fbaa8
Tiago Silva <tiago.silva.1996@hotmail.com>
2018-02-21 15:28:33 +0000

    experience controller + beautifications

--- partials/experience/experience.controller.js
+++ partials/experience/experience.controller.js
@@ -3,12 +3,20 @@ app.controller('ExperienceCtrl', function () {
 	vm.projects = [];
 	var device = {
 		name:'Device Cordinator',
-		points : ['dirver','blabla'] 
+		points : ['BackEnd Development for a web application in the pharmacy market','Web Socket usage for an asynchronous and bidirecional communication',
+		'Tcp-Ip protocol communication to trigger events with pharmacy hardware','C# wrappers from c++ and delphi dlls','Duration 3 months'] 
 	};
 	var device1 = {
-		name:'Device Cordinatordd',
-		points : ['dirver','blabla'] 
+		name:'Estimation Tool',
+		points : ['Web Application to calculate future IT Consulting projects cost in human hours','Built a SQL DataBase in SqlServer2014 Management Studio ',
+		'Entity Framework ORM','Developed Web Api in ASP.NET','Used Angular JS Framework for a single page application',
+		'Duration 2-3 months'] 
 	};
+	var mercedes ={
+		name:'Mercedes WebService',
+		points :['WebService development for pdf invoice generator','WCF WebService experience','iTextSharp library experience with one of the authors','Duration 3 weeks']
+	}
 	vm.projects.push(device); 
 	vm.projects.push(device1); 
+	vm.projects.push(mercedes); 
 });
--- partials/hobbies/hobbies.controller.js
+++ partials/hobbies/hobbies.controller.js
@@ -7,14 +7,29 @@ app.controller('HobbiesCtrl', function () {
 	};
 	var bananaBreak = {
 		image:'images/cycling/bananaBreak.jpg',
-		description: 'Bananinha'
+		description: 'Banana break'
 	};
 	var subir = {
 		image:'images/cycling/boss.jpg',
-		description: 'Subir'
+		description: 'Pushing myself at the start of a climb'
+	};
+	var trilho = {
+		image:'images/cycling/cavalinhos.jpg',
+		description: "Landscape at one of the Ericeira's trail"
+	};
+	var paragem = {
+		image:'images/cycling/sobreiro.jpg',
+		description: 'Beer break at a local Festival'
+	};
+	var chuva = {
+		image:'images/cycling/chuva.jpg',
+		description: 'Trail completly flooded'
 	};
 	cycling.pictures.push(bananaBreak);
 	cycling.pictures.push(subir);
+	cycling.pictures.push(trilho);
+	cycling.pictures.push(paragem);
+	cycling.pictures.push(chuva);
 	vm.hobbies.push(cycling);
-	console.log(vm.hobbies);
+	console.log(cycling);
 });
